b3846c7c3b3e38ac26dfb1d43150c3989c8f9144
ryanr1507 <ryanr1507@gmail.com>
2021-09-10 13:47:44 -0400

    Updates

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 //Check what environment we are in and decide which keys to use
-if(process.env.NOD_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     module.exports = require('./keys_prod');
 } else {
     module.exports = require('./keys_dev');
